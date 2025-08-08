0f2d3ccd593976e2cf2d7b63f9b601b03df9c372
Lanz Alexander Malto <lmalto382@gmail.com>
2025-08-08 23:12:41 +0800

    Pushing initial files

---
+++ src/components/Header.jsx
@@ -0,0 +1,17 @@
+const Header = () => {
+  return (
+    <header className="mt-4 w-2/3 rounded-2xl mx-auto py-6 bg-ash-blue">
+      <ul className="flex justify-center gap-14">
+        <li></li>
+        <li>About</li>
+        <li>Projects</li>
+        <li>Extras</li>
+        <li>Skills</li>
+        <li>Contact</li>
+        <li>Resume</li>
+      </ul>
+    </header>
+  );
+};
+
+export default Header;
